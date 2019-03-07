32109faf63f6452ac7b4cd950c5e5f760b4f73d6
Jop Bouckaert <jop.bouckaert@outlook.com>
2019-03-07 20:15:19 +0100

    add websocket support

--- broker.js
+++ broker.js
@@ -1,7 +1,8 @@
 const mosca = require('mosca')
 
 var moscaSettings = {
-    port: 1883
+    port: 1883,
+    http: {port: 9001, bundle: true, static: './'}
 };
 
 var server = new mosca.Server(moscaSettings);
